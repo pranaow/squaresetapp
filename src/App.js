23ede44cb9b13a0a9a402e2806740df4024d26d1
pwalekar <pwalekar@ces-ltd.com>
2020-07-22 12:01:56 +0530

    working implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
 import SquareSet from './SquareSet.js';
+import ClearCache from "react-clear-cache";
 
 function App() {
   return (
     <div className="App">
+      <ClearCache>
+        {({ isLatestVersion, emptyCacheStorage }) => (
+          <div>
+            {!isLatestVersion && (
+              <p>
+                // eslint-disable-next-line 
+                <a
+                  href="#"
+                  onClick={e => {
+                    e.preventDefault();
+                    emptyCacheStorage();
+                  }}
+                >
+                  Update version
+                </a>
+              </p>
+            )}
+          </div>
+        )}
+      </ClearCache>
       <SquareSet debug="1"/>
     </div>
   );
--- src/SquareSet.js
+++ src/SquareSet.js
@@ -1,22 +1,26 @@
 import React from "react";
 import Square from './Square.js'
+import axios from 'axios';
 
 class SquareSet extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squareset: this.props.squareset
+            scale: this.props.scale,
+            subsections: this.props.subsections,
+            panels : this.props.panels,
+            cabinet : this.props.cabinet
         }
     }
 
     removeElement = (idx) => {
         console.log(idx)
-        const squareset = Object.assign([],this.state.squareset);
-        squareset.splice(idx,1);
-        console.log(JSON.stringify(squareset))
-        this.setState({squareset:squareset})
+        const subsections = Object.assign([], this.state.subsections);
+        subsections.splice(idx, 1);
+        console.log(JSON.stringify(subsections))
+        this.setState({ subsections: subsections })
         // this.setState({
-        //     squareset: this.state.squareset.filter((person, index) => {
+        //     subsections: this.state.subsections.filter((person, index) => {
         //         if (index !== idx) {
 
         //             return person
@@ -27,47 +31,131 @@ class SquareSet extends React.Component {
 
     addElement = () => {
         this.setState({
-            squareset: this.state.squareset.concat([{width : 11, height:12}])
+            subsections: this.state.subsections.concat([{ width: 38, config: 0, ratio: [1,1,0,0,0] }])
         });
     }
 
     handleChange = (idx, event) => {
-        const squareset = Object.assign([],this.state.squareset);
-        squareset[idx] = event;
-        this.setState({squareset:squareset})
+        const subsections = Object.assign([], this.state.subsections);
+        subsections[idx] = event;
+        this.setState({ subsections: subsections })
         console.log(event);
     }
 
     showState = () => {
-        alert(JSON.stringify(this.state));
+        const inputJSON = {...this.state}
+        console.log(inputJSON);
+        axios.post('https://pranaowalekar.pythonanywhere.com/generateCabinet', inputJSON).then(response => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', 'file.svg'); //or any other extension
+            document.body.appendChild(link);
+            link.click();
+        }).catch(error => {
+            alert("Something went wrong. Please try again later.");
+        });
+    }
+
+    handleChangeMock = (idx, event) => {
+        const subsections = Object.assign([], this.state.subsections);
+        subsections[idx][event.target.name] = parseFloat(event.target.value);
+        this.setState({ subsections: subsections })
+        console.log(JSON.stringify(this.state))
+    }
+
+    handleChangeMockHL = (event) => {
+        if (event.target.name === "thickness"){
+            let a = {
+                thickness : parseFloat(event.target.value)
+            }
+            this.setState({ panels:  a})
+        }else if(event.target.name === "cabDepth"){
+            let a = Object.assign({}, this.state.cabinet);
+            a["depth"] = parseFloat(event.target.value)
+            console.log(JSON.stringify(a));
+            this.setState({cabinet:a})
+        }
+        else{
+            this.setState({ [event.target.name]: parseFloat(event.target.value) })
+        }
+    }
+
+    ratioChange = (idx, idx1, event) => {
+        const ratio = this.state.subsections[idx].ratio
+        ratio[idx1] = parseFloat(event.target.value)
+        if (isNaN(parseFloat(event.target.value)))
+            ratio[idx1] = ""
+        this.setState({ratio : {...this.state.subsections[idx], ratio:ratio}})
+        console.log(this.state)
     }
 
     render() {
-        console.log(JSON.stringify(this.state))
+
         return (
             <div>
-                <button onClick={this.showState}>Show Status</button>
-                <button onClick={this.addElement}>add</button>
+                <h1>Generate Paper templates for miniature cabinets</h1>
+                <button onClick={this.showState}>Download Template</button>
+                
+                <br></br>
+                Scale:<input type="text" name="scale" defaultValue={this.state.scale} onChange={this.handleChangeMockHL} /> Model/Actual
+                <br />
+                <h2>Panels</h2>
+                Thickness (mm):<input type="text" name="thickness" defaultValue={this.state.scale} onChange={this.handleChangeMockHL} />
+                <br />
+                <h2>Cabinet</h2>
+                Depth (mm):<input type="text" name="cabDepth" defaultValue={this.state.cabinet.depth} onChange={this.handleChangeMockHL} />
+                <br/>
+                Height (mm):<input type="text" name="cabHeight" defaultValue={this.state.cabinet.height} onChange={this.handleChangeMockHL} />
+                <br></br>
+                <h2>Subsections<button onClick={this.addElement}>+</button></h2>
                 {
-                    this.state.squareset.map((square,idx)=>(
-                        <Square debug="1" key={idx} name={idx} square={square} handleValueChange={this.handleChange} removeChild={this.removeElement}/>
+
+
+                    this.state.subsections.map((subsection, idx) => (
+                        <>
+                            <h3>Section {idx + 1} <button onClick={this.removeElement.bind(this, idx)}>-</button></h3>
+                            
+                        Width (mm):<input type="text" key={idx} name="width" value={subsection.width} onChange={this.handleChangeMock.bind(this, idx)} />
+                            <br />
+                        Horizontal:<input type="text" key={idx + 50} name="config" value={subsection.config} onChange={this.handleChangeMock.bind(this, idx)} />
+
+                            <br />
+                            Panel Ratios
+                            {
+                                subsection.ratio.map((num, idx1) => (
+                                    <>
+                                    <input type="text" onChange={this.ratioChange.bind(this, idx, idx1)} value={num} style={{width : "2em"}}/>
+                                    {(idx1 !== subsection.ratio.length-1) && ":"}
+                                    </>
+                                ))
+                            }
+                        </>
+
                     ))
                 }
-                
+
             </div>)
     }
 }
 
 SquareSet.defaultProps = {
-    squareset: [{
-        width: 10,
-        height: 10
+    scale: 1,
+    cabinet : {
+        depth : 36,
+        height: 38
+    },
+    panels: {
+        thickness: 2
+    },
+    subsections: [{
+        width: 38,
+        config: 1,
+        ratio : [1,1,0,0,0]
     }, {
-        width: 20,
-        height: 20
-    }, {
-        width: 30,
-        height: 30
+        width: 38,
+        config: 0,
+        ratio : [1,1,0,0,0]
     }]
 }
 
